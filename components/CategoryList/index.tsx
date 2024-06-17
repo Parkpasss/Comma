23c591c784c9116a3dac18d9c4c21a05f58ab86e
Park Jina <parkpass_@naver.com>
2024-06-17 18:22:45 +0900

    change list px

--- components/CategoryList/index.tsx
+++ components/CategoryList/index.tsx
@@ -10,9 +10,9 @@ import { BiReset } from "react-icons/bi"
 export default function CategoryList() {
   const [filterValue, setFilterValue] = useRecoilState(filterState)
   return (
-    <div className="flex gap-6 fixed top-20 inset-x-0 mx-auto overflow-x-scroll w-full flex-nowrap px-2 sm:pl-24 pr-16 bg-white z-10 mb-6 justify-center">
+    <div className="flex gap-6 fixed top-20 inset-x-0 mx-auto overflow-x-scroll w-full flex-nowrap px-4 sm:pl-24 pr-16 bg-white z-10 mb-6 justify-center">
       <button
-        className="flex-none justify-center gap-3 py-4 w-32 text-center"
+        className="flex-none justify-center gap-3 py-4 w-16 text-center"
         onClick={() => {
           setFilterValue({
             ...filterValue,
